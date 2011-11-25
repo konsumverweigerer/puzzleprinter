425753630e15953e9179aa14d957a4639129cba1
Peter <peter.gruber@revenupath.com>
2011-11-25 10:17:24 +0100

    add puzzle

--- puzzles/models.py
+++ puzzles/models.py
@@ -6,10 +6,18 @@ class Order(models.Model):
         (u'P', u'Processing'),
         (u'F', u'Finished'),
     )
-    SHIPPING= (
+    SHIPPING = (
         (u'N', u'New'),
         (u'S', u'Shipping'),
-        (u'T', u'Reched Target'),
+        (u'T', u'Reached Target'),
+    )
+    SHOPSYNC = (
+        (u'N', u'Not synchronized'),
+        (u'S', u'Synchronized'),
+    )
+    PRINTSYNC = (
+        (u'N', u'Not synchronized'),
+        (u'S', u'Synchronized'),
     )
     order_status = models.CharField(max_length=4,choices=STATUS)
     order_id = models.CharField(max_length=64)
@@ -21,12 +29,16 @@ class Order(models.Model):
     shipping_city = models.CharField(max_length=255)
     shipping_country = models.CharField(max_length=255)
     shipping_status = models.CharField(max_length=4,choices=SHIPPING)
+    shipping_tracking = models.CharField(max_length=4,choices=TRACKING)
+    shopsync = models.CharField(max_length=4,choices=SHOPSYNC)
+    printsync = models.CharField(max_length=4,choices=PRINTSYNC)
 
 class Puzzle(models.Model):
     puzzle_id = models.CharField(max_length=64)
     puzzle_type = models.CharField(max_length=64)
     puzzle_template = models.CharField(max_length=64)
     puzzle_title = models.CharField(max_length=255)
+    printing_status = models.CharField(max_length=1000)
     order = models.ForeignKey(Order)
 
 class Image(models.Model):
--- puzzles/printer.py
+++ puzzles/printer.py
@@ -13,6 +13,9 @@ from PIL import Image
 if not os.path.exists(PRINTERDIR):
   os.mkdir(PRINTERDIR)
 
+def rendercover(template,canvas,image,title,width,height):
+    pass
+
 class MyConfigParser(ConfigParser.SafeConfigParser):
     def write(self, fp):
         if self._defaults:
@@ -29,26 +32,27 @@ class MyConfigParser(ConfigParser.SafeConfigParser):
             fp.write("\r\n")ser):
 
 class Order:
-    order_id=""
-    puzzle_type="1000"
-    puzzle_s3=None
-    puzzle_data=None
-    puzzle_title="Mein Puzzle"
-    state="ODR"
-    template="std"
-    shipping_name=""
-    shipping_street=""
-    shipping_number=""
-    shipping_zipcode=""
-    shipping_city=""
-    shipping_country=""
-    additionaldata=None
+    order_id = ""
+    puzzle_type = "1000"
+    puzzle_s3 = None
+    puzzle_data = None
+    puzzle_title = "Mein Puzzle"
+    state = "ODR"
+    template = "std"
+    shipping_name = ""
+    shipping_street = ""
+    shipping_number = ""
+    shipping_zipcode = ""
+    shipping_city = ""
+    shipping_country = ""
+    additionaldata = None
+    printing_status = None
 
     def generatebarcode(self):
-        if len(PRINTERSN)==2:
-            return PRINTERSN+str(int(md5.md5(order_id).hexdigest(),16)%10000000000)
-        elif len(PRINTERSN)==3:
-            return "0"+PRINTERSN+str(int(md5.md5(order_id).hexdigest(),16)%100000000)
+        if len(PRINTERKN)==2:
+            return PRINTERKN+str(int(md5.md5(order_id).hexdigest(),16)%10000000000)
+        elif len(PRINTERKN)==3:
+            return "0"+PRINTERKN+str(int(md5.md5(order_id).hexdigest(),16)%100000000)
         return ""
 
     def generateboktype(self):
@@ -73,11 +77,17 @@ class Order:
                 dimensions = t[2]
         puzzle = ""
         puzzleio = StringIO.StringIO()
+        coverio = StringIO.StringIO()
+        image = Image.open(self.getimage())
         c = Canvas(puzzleio,pagesize=(dimensions[0]*mm,dimensions[1]*mm))
-        c.drawInlineImage(Image.open(self.getimage()),0,0,width=dimensions[0]*mm,height=dimensions[1]*mm)
+        c.drawInlineImage(image,0,0,width=dimensions[0]*mm,height=dimensions[1]*mm)
         c.showPage()
         c.save()
-        return (puzzleio.buf,"")
+        c = Canvas(coverio,pagesize=(dimensions[2]*mm,dimensions[3]*mm))
+        rendercover(self.template,c,image,self.puzzle_title,dimensions[2],dimensions[3])
+        c.showPage()
+        c.save()
+        return (puzzleio.buf,coverio.buf)
 
     def write(self):
         if "ODR"!=state:
@@ -94,9 +104,11 @@ class Order:
         (puzzle,cover) = self.createpuzzle(basename)
         open(puzzlepdf,"w").write(puzzle)
         open(coverpdf,"w").write(cover)
+
         data.add_section("Order")
         data.set("Order","CustomersShortName",PRINTERSN)
         data.set("Order","CustomersOrderNumber",self.order_id)
+
         data.add_section("Book")
         data.set("Book","BarcodeNumber",basename)
         data.set("Book","BookType",self.generateboktype())
