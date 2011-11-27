84428c2568a561f12960aec90f28316db5a5889f
Peter <peter.gruber@revenupath.com>
2011-11-27 11:02:31 +0100

    more printing stuff

--- puzzles/printer.py
+++ puzzles/printer.py
@@ -1,17 +1,14 @@
-from puzzle-settings import *
+from puzzlesettings import *
 
 import ConfigParser,StringIO
 from boto.utils import fetch_file
-import md5,os
+import md5,os,ftplib
 from reportlab.lib import colors
 from reportlab.lib.units import inch, cm, mm
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import Paragraph, Frame
 from PIL import Image
-
-if not os.path.exists(PRINTERDIR):
-  os.mkdir(PRINTERDIR)
 
 def rendercover(template,canvas,image,title,width,height):
     pass
@@ -29,7 +26,7 @@ class MyConfigParser(ConfigParser.SafeConfigParser):
                 if key != "__name__":
                     fp.write("%s = %s\r\n" %
                              (key, str(value).replace('\n', '\r\n\t')))
-            fp.write("\r\n")ser):
+            fp.write("\r\n")
 
 class Order:
     order_id = ""
@@ -47,6 +44,7 @@ class Order:
     shipping_country = ""
     additionaldata = None
     printing_status = None
+    shipping_status = None
 
     def generatebarcode(self):
         if len(PRINTERKN)==2:
@@ -55,7 +53,7 @@ class Order:
             return "0"+PRINTERKN+str(int(md5.md5(order_id).hexdigest(),16)%100000000)
         return ""
 
-    def generateboktype(self):
+    def generatebooktype(self):
         for t in PUZZLETYPES:
             if t[0]==self.puzzle_type:
                 return t[1]
@@ -92,56 +90,118 @@ class Order:
     def write(self):
         if "ODR"!=state:
             return
-        basename = self.generatebarcode()
-        filename = basename+"."+self.state
-        tmpname = basename+".TMP"
+        ftp = ftplib.FTP(PRINTERSRV,PRINTERFTPUSER,PRINTERFTPPWD)
+        if not ftp:
+            return
+        try:
+            basename = self.generatebarcode()
+            filename = basename+"."+self.state
+            tmpname = basename+".TMP"
+            data = MyConfigParser()
+            puzzlepdf = "I"+basename+".pdf"
+            coverpdf = "U"+basename+".pdf"
+            if not os.path.exists(dirname):
+                ftp.mkd(basename)
+            (puzzle,cover) = self.createpuzzle(basename)
+
+            ftp.cwd("/")
+            ftp.cwd(basename)
+            ftp.storbinary("STOR "+puzzlepdf,StringIO.StringIO(puzzle))
+            ftp.storbinary("STOR "+coverpdf,StringIO.StringIO(cover))
+
+            data.add_section("Order")
+            data.set("Order","CustomersShortName",PRINTERSN)
+            data.set("Order","CustomersOrderNumber",self.order_id)
+
+            data.add_section("Book")
+            data.set("Book","BarcodeNumber",basename)
+            data.set("Book","BookType",self.generatebooktype())
+            data.set("Book","PageCount",1)
+            data.set("Book","BookCount",1)
+            data.set("Book","PdfNameBook",basename+"\\"+puzzlepdf)
+            data.set("Book","BookMd5",md5.md5(puzzle).hexdigest())
+            data.set("Book","PdfNameCover",basename+"\\"+coverpdf)
+            data.set("Book","CoverMd5",md5.md5(cover).hexdigest())
+
+            data.set("Book","DeliveryAddressCount",1)
+            data.set("Book","Delivery0BookCount",0)
+            data.set("Book","Delivery0ParcelService",0)
+            data.set("Book","Delivery0Name",self.shipping_name)
+            data.set("Book","Delivery0Street",self.shipping_street)
+            data.set("Book","Delivery0HouseNumber",self.shipping_number)
+            data.set("Book","Delivery0ZipCode",slef.shipping_zipcode)
+            data.set("Book","Delivery0City",self.shipping_city)
+            data.set("Book","Delivery0Country",self.shipping_country)
+
+            if self.additionaldata:
+                additionalpdf = "additional.pdf"
+                ftp.storbinary("STOR "+additionalpdf,StringIO.StringIO(additionaldata))
+                data.set("Book","Delivery0AdditionalDocuments",basename+"\\"+self.additionalpdf)
+            dataio = StringIO.StringIO()
+            data.write(dataio)
+            ftp.cwd("/")
+            ftp.storbinary("STOR "+tmpname,StringIO.StringIO(dataio))
+    #        ftp.rename(tmpname,filename)
+        finally:
+            ftp.quit()
+
+    def read(self,fn,fp,statusfp=[]):
         data = MyConfigParser()
-        puzzlepdf = os.path.join(basename,"I"+basename+".pdf")
-        coverpdf = os.path.join(basename,"U"+basename+".pdf")
-        dirname = os.path(PRINTERDIR,basename)
-        if not os.path.exists(dirname):
-            os.mkdir(dirname)
-        (puzzle,cover) = self.createpuzzle(basename)
-        open(puzzlepdf,"w").write(puzzle)
-        open(coverpdf,"w").write(cover)
+        data.readfp(fp)
+        self.state = fn[-3:]
+        self.order_id = data.get("Order","CustomersOrderNumber")
+        self.shipping_name = data.get("Book","Delivery0Name")
+        self.shipping_street = data.get("Book","Delivery0Street")
+        self.shipping_number = data.get("Book","Delivery0HouseNumber")
+        self.shipping_zipcode = data.get("Book","Delivery0ZipCode")
+        self.shipping_city = data.get("Book","Delivery0City")
+        self.shipping_country = data.get("Book","Delivery0Country")
+        for sfp in statusfp:
+            status = MyConfigParser()
+            status.readfp(StringIO.StringIO(sfp))
+            self.shipping_status = status.get("BookStates",fn[:-4])
+            self.printing_status = status.get("ShippingInfo",fn[:-4]+"_0")
+            if (not self.printing_status) and (not self.shipping_status):
+                break
 
-        data.add_section("Order")
-        data.set("Order","CustomersShortName",PRINTERSN)
-        data.set("Order","CustomersOrderNumber",self.order_id)
-
-        data.add_section("Book")
-        data.set("Book","BarcodeNumber",basename)
-        data.set("Book","BookType",self.generateboktype())
-        data.set("Book","PageCount",1)
-        data.set("Book","BookCount",1)
-        data.set("Book","PdfNameBook",puzzlepdf.replace("/","\\"))
-        data.set("Book","BookMd5",md5.md5(puzzle).hexdigest())
-        data.set("Book","PdfNameCover",coverpdf.replace("/","\\"))
-        data.set("Book","CoverMd5",md5.md5(cover).hexdigest())
-
-        data.set("Book","DeliveryAddressCount",1)
-        data.set("Book","Delivery0BookCount",0)
-        data.set("Book","Delivery0ParcelService",0)
-        data.set("Book","Delivery0Name",self.shipping_name)
-        data.set("Book","Delivery0Street",self.shipping_street)
-        data.set("Book","Delivery0HouseNumber",self.shipping_number)
-        data.set("Book","Delivery0ZipCode",slef.shipping_zipcode)
-        data.set("Book","Delivery0City",self.shipping_city)
-        data.set("Book","Delivery0Country",self.shipping_country)
-
-        if self.additionaldata:
-            additionalpdf = os.path.join(basename,"additional.pdf")
-            open(additionalpdf,'w').write(self.additionaldata)
-            data.set("Book","Delivery0AdditionalDocuments",self.additionalpdf.replace("/","\\"))
-        data.write(open(os.path.join(PRINTERDIR,tmpname),'w'))
-        os.rename(os.path.join(PRINTERDIR,tmpname),os.path.join(PRINTERDIR,filename))
-
-    def read(self,filename):
-        data = MyConfigParser()
-        pass
-
-    @static
-    def fromFile(filename):
+    @staticmethod
+    def fromFile(fn,data,statusdata):
         o = Order()
-        o.read(filename)
+        o.read(StringIO.StringIO(data),StringIO.StringIO(statusdata))
         return o
+
+def readorders(status=['ODR','FLT','WRK','ACC']):
+    ftp = ftplib.FTP(PRINTERSRV,PRINTERFTPUSER,PRINTERFTPPWD)
+    orders = []
+    if not ftp:
+        return
+    try:
+        files = ftp.nlst()
+        statuslist = [x for x in files if x.endsWith(".STA")]
+        statuslist.sort(reverse=True)
+        statusio = StringIO.StringIO()
+        status = []
+        for fn in statuslist:
+            ftp.retrbinary("RETR "+fn,lambda x:statusio.write(x))
+            status.append(statusio.buf)
+        for fn in [x for x in files if x[-3:] in status]:
+            v = StringIO.StringIO()
+            ftp.retrbinary("RETR "+fn,lambda x:v.write(x))
+            orders.append(Order.fromFile(fn,v.buf,status))
+    finally:
+        ftp.quit()
+    return orders
+
+def putorder(orderid,s3,template,address={}):
+    order = Order()
+    order.order_id = orderid
+    order.puzzle_s3 = s3
+    order.template = template
+    order.shipping_name = address["name"]
+    order.shipping_street = address["street"]
+    order.shipping_number = address["number"]
+    order.shipping_zipcode = address["zipcode"]
+    order.shipping_city = address["city"]
+    order.shipping_country = address["country"]
+    order.write()
+
