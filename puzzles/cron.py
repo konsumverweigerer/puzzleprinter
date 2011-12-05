1e9cb62321a37cb29a55a836c0560f09f052786d
Peter <peter.gruber@revenupath.com>
2011-12-05 18:07:30 +0100

    add debug

--- puzzles/cron.py
+++ puzzles/cron.py
@@ -1,6 +1,6 @@
 from django_cron import cronScheduler, Job
 from puzzles.models import *
-import logging
+import logging,random
 
 logger = logging.getLogger(__name__)
 
@@ -8,8 +8,10 @@ class Synchronize(Job):
     run_every = 20
                                                                     
     def job(self):
-        logger.error("run job")
+        t = random.randint(0,100000)
+        logger.error("runnig job: "+t)
         syncer.syncall()
+        logger.error("run job: "+t)
 
 cronScheduler.register(Synchronize)
 
