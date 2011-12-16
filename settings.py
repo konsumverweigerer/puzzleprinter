23082bc718178018a50bfb5b6f4217c16854cd74
Peter <peter.gruber@revenupath.com>
2011-12-16 18:15:28 +0100

    remove cron

--- settings.py
+++ settings.py
@@ -120,7 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'django_cron',
+    #'django_cron',
     'puzzles',
 )
 
