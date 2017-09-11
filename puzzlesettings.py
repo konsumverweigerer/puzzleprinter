1f85df014376b80c59aadffd868fc1d6acdb34de
Peter <peter.gruber@revenupath.com>
2017-09-12 01:23:58 +0200

    add config template

---
+++ puzzlesettings.py
@@ -0,0 +1,68 @@
+BASEDIR = ""
+
+SHOPIFY_API_KEY = ""
+SHOPIFY_API_SECRET = ""
+
+#URL format 	https://apikey:password@hostname/admin/resource.xml
+#Example URL 	https://45c4be1d0c400a4a2600f8f17d666b63:40d53e7ff2b763ebca47d40b8aea46ec@puzzleprinter.myshopify.com/admin/orders.xml
+SHOPIFYSHOP = ""
+SHOPIFYKEY = ""
+SHOPIFYPWD = ""
+SHOPIFYSEC = ""
+
+SHOPLOGIN = ""
+SHOPPWD = ""
+SHOPINVOICEURL = ""
+SHOPINVOICEAPIURL = ""
+INVOICEID = ""
+
+AWSKEYID = ""
+AWSSECRET = ""
+AWSREGION = ""
+AWSPATH = ""
+AWSBUCKET = ""
+AWSARCHIVE = ""
+
+# Anzahl Teile (Kurzname), Book ID, Dimensionen (Puzzle,Schachtel), Schachtelbedruckung (m:Mitte,b:Unten,r:Rechts,l:Links,t:Oben) (Rotation,Translation,Groesse)
+PUZZLETYPES = (
+    ('1000','P1000_PZ_N_I6U6V0_ML_P',(660,485,514,414),{'m':(0,83,80,348,254),'b':(0,83,15,348,55),'r':(90,80,-496,254,55),'t':(180,-431,-399,348,55),'l':(270,-334,18,254,55)}),
+    ('500','P500_PZ_N_I6U6V0_ML_P',(485,330,434,328),{'m':(0,55,53,324,222),'b':(0,55,13,324,35),'r':(90,53,-419,222,35),'t':(180,-379,-315,324,35),'l':(270,-275,15,222,35)}),
+    ('200','P200_PZ_N_I6U6V0_ML_P',(430,307,434,328),{'m':(0,55,53,324,222),'b':(0,55,13,324,35),'r':(90,53,-419,222,35),'t':(180,-379,-315,324,35),'l':(270,-275,15,222,35)}),
+    ('600','Herz600_PZ_N_I6U6V0_ML_P',(569,485,514,414))
+)
+
+COLORTABLE = (
+    ("1","#333333"),
+    ("2","#FFFFFF"),
+    ("3","#7D0E19"),
+    ("4","#DD5B78"),
+    ("5","#FF8E1D"),
+    ("6","#FFFF00"),
+    ("7","#91B329"),
+    ("8","#1B8B34"),
+    ("9","#003E6F"),
+    ("10","#2DC0C8"),
+)
+
+PUZZLETABLE = (
+    ("1","100"),
+    ("2","200"),
+    ("3","500"),
+    ("4","1000"),
+)
+
+ORIENTATIONTABLE = (
+    ("1","horizontal"),
+    ("2","vertical"),
+)
+
+TEMPLATETABLE = (
+    ("1","std"),
+)
+
+PRINTERSN = ""
+PRINTERKN = ""
+
+PRINTERSRV = ""
+PRINTERFTPUSER = ""
+PRINTERFTPPWD = ""
